5e408d1f13ba744602c32f218fb89baecdc728c2
Lenar Saitov <lenarsaitov1@yandex.ru>
2021-02-20 11:49:39 +0500

    creating a put_order method

--- order_book.py
+++ order_book.py
@@ -7,8 +7,25 @@ class OrderBook:
         self.bid_prices = []
         self.bid_quantity = []
 
+        self.bids = []
+        self.asks = []
+
+        self.initial_order = []
+        self.spred = 0
+
+    @property
+    def max_bid(self):
+        return max(self.bids) if self.bids else 0
+
+    @property
+    def min_ask(self):
+        return min(self.asks) if self.asks else float('inf')
+
     def put_order(self, order):
-        pass
+        if order.side == 0:
+            self.bids[order.price].append(order)
+        else:
+            self.asks[order.price].append(order)
 
     def delete_order(self, index):
         pass
@@ -26,4 +43,8 @@ class Order:
 if __name__ == '__main__':
     ob = OrderBook()
 
-    Order(0, 12.23, 10)
+    order1 = Order(1, 61.57, 10)
+    order2 = Order(0, 57.23, 10)
+
+    ob.initial_order.append(order1)
+    ob.put_order(ob.initial_order[0])
